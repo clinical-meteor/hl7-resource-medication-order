117ccd910afedad8a28c1e34c80bb3ba71178825
Abigail Watson <awatson1978@gmail.com>
2018-02-12 22:46:37 -0600

    workflow refactor

---
+++ index.jsx
@@ -0,0 +1,32 @@
+
+
+import MedicationOrdersPage from './client/MedicationOrdersPage';
+import MedicationOrdersTable from './client/MedicationOrdersTable';
+import { MedicationOrder, MedicationOrders, MedicationOrderSchema } from './lib/MedicationOrders';
+
+var DynamicRoutes = [{
+  'name': 'MedicationOrdersPage',
+  'path': '/medication-orders',
+  'component': MedicationOrdersPage,
+  'requireAuth': true
+}];
+
+var SidebarElements = [{
+  'primaryText': 'Medication Orders',
+  'to': '/medication-orders',
+  'href': '/medication-orders'
+}];
+
+export { 
+  SidebarElements, 
+  DynamicRoutes, 
+
+  MedicationOrdersPage,
+  MedicationOrdersTable,
+
+  MedicationOrder,
+  MedicationOrders,
+  MedicationOrderSchema
+};
+
+
