0ee9a51c2fec5afedf94e00e28c99ebffd9391e0
Abigail Watson <abigailwatson@Abigails-Mac-mini-317772.local>
2018-05-04 22:15:43 -0500

    schema import on client

--- index.jsx
+++ index.jsx
@@ -22,11 +22,7 @@ export {
   DynamicRoutes, 
 
   MedicationOrdersPage,
-  MedicationOrdersTable,
-
-  MedicationOrder,
-  MedicationOrders,
-  MedicationOrderSchema
+  MedicationOrdersTable
 };
 
 
