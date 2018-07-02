42f57a16ef226aa335f5f659ebc08715265bb83d
Abigail Watson <abigailwatson@Abigails-Mac-mini-317772.local>
2018-07-01 19:46:31 -0500

    clinical:alert@2.1.3

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-medication-order',
-  version: '1.5.6',
+  version: '1.5.8',
   summary: 'HL7 FHIR Resource - MedicationOrder',
   git: 'https://github.com/clinical-meteor/hl7-resource-medication-order',
   documentation: 'README.md'
@@ -26,6 +26,7 @@ Package.onUse(function (api) {
   api.use('clinical:hl7-resource-datatypes@3.0.1');
   api.use('clinical:hl7-resource-bundle@1.3.10');
   api.use('matb33:collection-hooks@0.7.15');
+  api.use('clinical:alert@2.1.3');
 
   api.addFiles('lib/MedicationOrders.js', ['client', 'server']);
   api.addFiles('server/rest.js', 'server');
