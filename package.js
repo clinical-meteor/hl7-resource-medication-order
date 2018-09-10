8b3663a9db38f16a1f3e8145800e224caef95b65
Abigail Watson <abigailwatson@Abigails-Mac-mini-380428.local>
2018-09-09 20:58:20 -0500

    sync

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-medication-order',
-  version: '1.6.1',
+  version: '1.6.2',
   summary: 'HL7 FHIR Resource - MedicationOrder',
   git: 'https://github.com/clinical-meteor/hl7-resource-medication-order',
   documentation: 'README.md'
@@ -45,5 +45,7 @@ Package.onUse(function (api) {
 });
 
 Npm.depends({
-  "simpl-schema": "1.5.3"
+  "simpl-schema": "1.5.3",
+  "moment": "2.22.2",
+  "lodash": "4.17.4"
 })
